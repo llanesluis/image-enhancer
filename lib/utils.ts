8d413340962be6848de529e5f5336a5631dca1fb
Luis Llanes <luisllaboj@gmail.com>
2024-03-10 17:11:54 -0700

    handleError util function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,21 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function handleError(error: unknown) {
+  if (error instanceof Error) {
+    console.error(error.message);
+    throw new Error(`Error: ${error.message}`);
+  }
+
+  if (typeof error === "string") {
+    console.error(error);
+    throw new Error(`Error: ${error}`);
+  }
+
+  console.error(error);
+  throw new Error(`An unknown error occurred: ${JSON.stringify(error)}`);
 }
