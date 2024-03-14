724aac5d6e645c2e072fa2f7ac29ca5e471a3213
Luis Llanes <luisllaboj@gmail.com>
2024-03-13 21:35:37 -0700

    add transformation page

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,22 +1,37 @@
 import Header from "@/components/shared/Header";
-import {
-  TransformationType,
-  transformationsTypes,
-} from "@/constants/transformations";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { transformationsTypes } from "@/constants/transformations";
+import { getUserById } from "@/lib/actions/user.actions";
+import { TransformationTypeKey } from "@/types/transformation";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 interface AddTransformationTypeProps {
   params: {
-    type: TransformationType;
+    type: TransformationTypeKey;
   };
 }
 
-export default function AddTransformationType({
+export default async function AddTransformationType({
   params: { type },
 }: AddTransformationTypeProps) {
-  const { title, subtitle, icon } = transformationsTypes[type];
+  const { userId } = auth();
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
+  if (!user) throw new Error("User not found");
+
+  const { title, subtitle } = transformationsTypes[type];
+
   return (
     <main className="container py-16">
       <Header title={title} subtitle={subtitle} />
+      <TransformationForm
+        action="Create"
+        userId={user._id}
+        type={type}
+        creditBalance={user.creditBalance || 0}
+      />
     </main>
   );
 }
