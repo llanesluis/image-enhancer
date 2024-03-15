63c943d587dd1a3eeef128b7b0b8b333e6a0197f
Luis Llanes <luisllaboj@gmail.com>
2024-03-14 19:33:52 -0700

    refactorizacion del custom form field y la utilidad para combinar objetos

---
+++ components/shared/CustomFormField.tsx
@@ -0,0 +1,45 @@
+import { Control } from "react-hook-form";
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { TransformationFormData } from "./TransformationFormRefs";
+
+export interface CustomFormFieldProps {
+  control: Control<TransformationFormData> | undefined;
+  render: (props: { field: any }) => React.ReactNode;
+  name: keyof TransformationFormData;
+  formLabel?: string;
+  formDescription?: string;
+  className?: string;
+}
+
+export const CustomFormField = ({
+  control,
+  render,
+  name,
+  formLabel,
+  formDescription,
+  className,
+}: CustomFormFieldProps) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className={className}>
+          {formLabel && <FormLabel>{formLabel}</FormLabel>}
+          <FormControl>{render({ field })}</FormControl>
+          {formDescription && (
+            <FormDescription>{formDescription}</FormDescription>
+          )}
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,12 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+//MERGE CLASSES
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+//HANDLING ERRORS
 export function handleError(error: unknown) {
   if (error instanceof Error) {
     console.error(error.message);
@@ -27,4 +29,30 @@ export const debounce = (func: (...args: any[]) => void, delay: number) => {
     if (timeoutId) clearTimeout(timeoutId);
     timeoutId = setTimeout(() => func.apply(null, args), delay);
   };
+};
+
+// DEEP MERGE OBJECTS
+export const deepMergeObjects = (obj1: any, obj2: any) => {
+  if (obj2 === null || obj2 === undefined) {
+    return obj1;
+  }
+
+  let output = { ...obj2 };
+
+  for (let key in obj1) {
+    if (obj1.hasOwnProperty(key)) {
+      if (
+        obj1[key] &&
+        typeof obj1[key] === "object" &&
+        obj2[key] &&
+        typeof obj2[key] === "object"
+      ) {
+        output[key] = deepMergeObjects(obj1[key], obj2[key]);
+      } else {
+        output[key] = obj1[key];
+      }
+    }
+  }
+
+  return output;
 };
