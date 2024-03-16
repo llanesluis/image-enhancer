2a50ae6fbb9b4b77a4c41676839e7d366dc83f3c
Luis Llanes <luisllaboj@gmail.com>
2024-03-15 20:37:34 -0700

    componentes para subir imagen y mostrar resultado

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -15,7 +15,7 @@ import { useToast } from "../ui/use-toast";
 
 interface MediaUploaderProps {
   onChange: (value: string) => void;
-  setImage: Dispatch<SetStateAction<IImage | null>>;
+  setImageData: Dispatch<SetStateAction<IImage | null>>;
   publicId: string;
   imageData: IImage | null;
   type: TransformationTypeKey;
@@ -24,14 +24,14 @@ interface MediaUploaderProps {
 export default function MediaUploader({
   imageData,
   onChange,
-  setImage,
+  setImageData,
   publicId,
   type,
 }: MediaUploaderProps) {
   const { toast } = useToast();
 
   const handleUploadSuccess = (result: any) => {
-    setImage((prevState: any) => ({
+    setImageData((prevState: any) => ({
       ...prevState,
       publicId: result?.info?.public_id,
       width: result?.info?.width,
@@ -45,7 +45,7 @@ export default function MediaUploader({
       title: "La imagen se subió correctamente",
       description: "Se ha utilizado 1 crédito",
       variant: "default",
-      duration: 5000,
+      duration: 3000,
     });
   };
 
@@ -54,7 +54,7 @@ export default function MediaUploader({
       title: "Ocurrió un error al subir la imagen",
       description: "Intenta de nuevo",
       variant: "destructive",
-      duration: 5000,
+      duration: 3000,
     });
   };
 
@@ -67,29 +67,28 @@ export default function MediaUploader({
     >
       {({ open }) => (
         <div className="flex flex-col gap-4">
-          <h3 className="font-confortaa text-4xl font-bold">Original</h3>
+          <h3 className="font-confortaa text-4xl font-bold max-md:text-3xl">
+            Original
+          </h3>
 
           {/* Si la imagen ya existe */}
-          {publicId && (
+          {publicId ? (
             <>
               <div className="size-fit cursor-pointer overflow-hidden rounded-[10px] border">
                 <CldImage
                   width={getImageSize(type, imageData, "width")}
                   height={getImageSize(type, imageData, "height")}
                   src={publicId}
-                  alt="image"
+                  alt="Imagen original"
                   sizes={"(max-width: 767px) 100vw, 50vw"}
                   placeholder={dataUrl as PlaceholderValue}
-                  className="media-uploader_cldImage"
+                  className="h-fit min-h-72 w-full rounded-[10px] border border-dashed object-cover"
                 />
               </div>
             </>
-          )}
-
-          {/* Si la imagen no existe, widget para subirla */}
-          {!publicId && (
+          ) : (
             <div
-              className="flex-center group flex h-72 cursor-pointer flex-col items-center gap-4 rounded-[16px]  border-2 border-dashed border-accentcolor bg-slate-200/20 p-12 shadow-inner hover:bg-accentcolor/10"
+              className="group flex h-72 cursor-pointer flex-col items-center justify-center  gap-4 rounded-[16px]  border-2 border-dashed border-accentcolor bg-slate-200/20 p-12 shadow-inner hover:bg-accentcolor/10"
               onClick={() => open()}
             >
               <div className=" rounded-[16px] bg-accentcolor bg-white p-4  shadow-sm shadow-accentcolor/50  transition-all ease-linear group-hover:opacity-70">
---
+++ components/shared/TransformedImage.tsx
@@ -0,0 +1,114 @@
+"use client";
+
+import { IImage } from "@/lib/database/models/image.model";
+import { TransformationTypeKey, Transformations } from "@/types/transformation";
+import { Button } from "../ui/button";
+import Image from "next/image";
+import { CldImage, getCldImageUrl } from "next-cloudinary";
+import { PlaceholderValue } from "next/dist/shared/lib/get-img-props";
+import { dataUrl, debounce, download, getImageSize } from "@/lib/utils";
+import { useState } from "react";
+
+interface TransformedImageProps {
+  image: IImage | null;
+  type: TransformationTypeKey;
+  title?: string;
+  isTransforming: boolean;
+  setIsTransforming?: React.Dispatch<React.SetStateAction<boolean>>;
+  transformationConfig: Transformations | null;
+}
+
+export default function TransformedImage({
+  image,
+  type,
+  transformationConfig,
+  isTransforming,
+  title,
+  setIsTransforming,
+}: TransformedImageProps) {
+  const imageTitle = title || image?.title;
+  const [isReadyToDownload, setIsReadyToDownload] = useState(false);
+
+  const handleDownload = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    event.preventDefault();
+    const cloudinaryUrl = getCldImageUrl({
+      width: image?.width,
+      height: image?.height,
+      src: image?.publicId!,
+      ...transformationConfig,
+    });
+
+    download(cloudinaryUrl, imageTitle || "imagen-modificada");
+  };
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex justify-between">
+        <h3 className="font-confortaa text-4xl font-bold max-md:text-3xl">
+          Modificada
+        </h3>
+
+        {isReadyToDownload && (
+          <Button
+            type="button"
+            variant={"outline"}
+            className="flex gap-2 border border-accentcolor"
+            onClick={handleDownload}
+          >
+            <Image
+              src="/assets/icons/download.svg"
+              alt="Descargar"
+              width={24}
+              height={24}
+              className="pb-[6px]"
+            />
+            Descargar
+          </Button>
+        )}
+      </div>
+
+      {image?.publicId && transformationConfig ? (
+        <div className="relative cursor-pointer overflow-hidden rounded-[10px] border">
+          <CldImage
+            width={getImageSize(type, image, "width")}
+            height={getImageSize(type, image, "height")}
+            src={image?.publicId}
+            alt={image?.title || "Imagen modificada"}
+            sizes={"(max-width: 767px) 100vw, 50vw"}
+            placeholder={dataUrl as PlaceholderValue}
+            className="h-fit min-h-72 w-full rounded-[10px] border border-dashed object-cover"
+            onLoad={() => {
+              setIsTransforming && setIsTransforming(false);
+              setIsReadyToDownload(true);
+            }}
+            onError={() => {
+              debounce(() => {
+                setIsTransforming && setIsTransforming(false);
+                setIsReadyToDownload(false);
+              }, 8000)();
+            }}
+            {...transformationConfig}
+          />
+
+          {isTransforming && (
+            <div className="absolute inset-0 z-20 ">
+              <Image
+                src="/assets/icons/spinner.svg"
+                width={50}
+                height={50}
+                alt="Cargando..."
+              />
+              <p className="text-white/80">Por favor espera...</p>
+            </div>
+          )}
+        </div>
+      ) : (
+        <div className="flex size-full items-center justify-center rounded-[16px]  border-2 border-dashed border-foreground/20 bg-slate-200/20 p-12 shadow-inner">
+          Imagen modificada
+        </div>
+      )}
+    </div>
+  );
+}
