32869afd8673a9c279915dc7d22842465f00f412
Luis Llanes <luisllaboj@gmail.com>
2024-03-15 20:36:31 -0700

    updatecredits de usuarios

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -65,3 +65,21 @@ export async function deleteUser(clerkId: string) {
     revalidatePath("/");
   }
 }
+
+export async function updateCreditBalance(userId: string, creditFee: number) {
+  try {
+    await connectToDatabase();
+
+    const updatedUserCredits = await User.findOneAndUpdate(
+      { _id: userId },
+      { $inc: { credits: creditFee } },
+      { new: true },
+    );
+
+    if (!updatedUserCredits) throw new Error("User credits update failed");
+
+    return JSON.parse(JSON.stringify(updatedUserCredits));
+  } catch (e) {
+    handleError(e);
+  }
+}
