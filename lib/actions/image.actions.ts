fb49ab0b851796509caf8c22c339d04af6491d98
Luis Llanes <luisllaboj@gmail.com>
2024-03-17 02:55:45 -0700

    get imagees serverr action

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -7,6 +7,7 @@ import { revalidatePath } from "next/cache";
 import Image, { IImage } from "../database/models/image.model";
 import User, { IUser } from "../database/models/user.model";
 import { redirect } from "next/navigation";
+import { v2 as cloudinary } from "cloudinary";
 
 export async function getImageById(imageId: string) {
   try {
@@ -26,6 +27,62 @@ export async function getImageById(imageId: string) {
   }
 }
 
+export async function getImages({ limit = 9, page = 1, searchQuery = "" }) {
+  try {
+    await connectToDatabase();
+    cloudinary.config({
+      cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+      secure: true,
+    });
+
+    let expression = "folder=pickuro";
+
+    if (searchQuery) expression += ` AND ${searchQuery}`;
+
+    const { resources } = await cloudinary.search
+      .expression(expression)
+      .execute();
+
+    const resourcesIds = resources.map((resource: any) => resource.public_id);
+
+    let query = {};
+
+    if (searchQuery) {
+      query = {
+        publicId: {
+          $in: resourcesIds,
+        },
+      };
+    }
+
+    const skipAmount = (Number(page) - 1) * limit;
+
+    const images = await Image.find(query)
+      .populate({
+        path: "author",
+        model: User,
+        select: "_id firstName lastName",
+      })
+      .sort({ updatedAt: -1 })
+      .skip(skipAmount)
+      .limit(limit);
+
+    if (!images) throw new Error("No se encontraron imagenes");
+
+    const totalImages = await Image.countDocuments(query);
+    const savedImages = await Image.countDocuments();
+
+    return {
+      data: JSON.parse(JSON.stringify(images)),
+      totalPages: Math.ceil(totalImages / limit),
+      savedImages,
+    };
+  } catch (error) {
+    handleError(error);
+  }
+}
 export async function addImage({ image, userId, path }: AddImageParams) {
   try {
     await connectToDatabase();
