d5fd728cbee190eb44f8cb579795b96d8599f01b
Luis Llanes <luisllaboj@gmail.com>
2024-03-17 01:26:03 -0700

    Texto del hero de las features

--- components/sections/Features.tsx
+++ components/sections/Features.tsx
@@ -6,10 +6,10 @@ import Link from "next/link";
 export default function Features() {
   return (
     <section id="features" className="bg border-y-4 border-dashed py-16">
-      <div className="bg-banner relative mb-8 flex h-72 flex-col items-center justify-center gap-8 rounded-xl bg-cover bg-no-repeat p-6 shadow-inner max-md:hidden">
+      <div className="bg-banner relative mb-8 flex h-72 flex-col items-center justify-center gap-8 rounded-[25px] bg-cover bg-no-repeat p-6 shadow-inner max-md:hidden ">
         <div className="absolute inset-0 z-0 bg-background/20" />
         <h1 className="z-10 text-center text-4xl font-semibold text-white lg:text-5xl">
-          Desata tu creatividad con{" "}
+          Pon a prueba tu creatividad con{" "}
           <span className="font-confortaa font-bold">Pickuro</span>
         </h1>
         <ul className="flex-center z-10 w-full gap-10">
