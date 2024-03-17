c8c56ca30f1729b1f5db66a2990e84d421805c76
Luis Llanes <luisllaboj@gmail.com>
2024-03-17 02:55:31 -0700

    utilidades para formatear url

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,8 +2,13 @@ import {
   AspectRatioOptionKey,
   aspectRatioOptions,
 } from "@/constants/transformations";
+import {
+  FormUrlQueryParams,
+  RemoveUrlQueryParams,
+} from "@/types/url-query-params";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import qs from "qs";
 
 //MERGE CLASSES
 export function cn(...inputs: ClassValue[]) {
@@ -120,25 +125,36 @@ export const download = (url: string, filename: string) => {
   } catch (error) {
     console.error(error);
   }
-
-  // if (!url) {
-  //   throw new Error("¡No se proporcionó la URL del recurso! Debes proporcionar una");
-  // }
-  // fetch(url)
-  //   .then((response) => response.blob())
-  //   .then((blob) => {
-  //     const blobURL = URL.createObjectURL(blob);
-  //     const a = document.createElement("a");
-
-  //     a.href = blobURL;
-
-  //     if (filename && filename.length) {
-  //       a.download = `${filename.replace(" ", "_")}.png`;
-  //       a.target = "_blank";
-  //     }
-
-  //     document.body.appendChild(a);
-  //     a.click();
-  //   })
-  //   .catch((error) => console.log({ error }));
 };
+
+// FORM URL QUERY
+export const formUrlQuery = ({
+  searchParams,
+  key,
+  value,
+}: FormUrlQueryParams) => {
+  const params = { ...qs.parse(searchParams.toString()), [key]: value };
+
+  return `${window.location.pathname}?${qs.stringify(params, {
+    skipNulls: true,
+  })}`;
+};
+
+// REMOVE KEY FROM QUERY
+export function removeKeysFromQuery({
+  searchParams,
+  keysToRemove,
+}: RemoveUrlQueryParams) {
+  const currentUrl = qs.parse(searchParams);
+
+  keysToRemove.forEach((key) => {
+    delete currentUrl[key];
+  });
+
+  // Remove null or undefined values
+  Object.keys(currentUrl).forEach(
+    (key) => currentUrl[key] == null && delete currentUrl[key],
+  );
+
+  return `${window.location.pathname}?${qs.stringify(currentUrl)}`;
+}
