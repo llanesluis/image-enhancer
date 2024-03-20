4c5ecd53b711cc40d2103ce44abb1a8c45a7e05f
Luis Llanes <luisllaboj@gmail.com>
2024-03-19 22:43:48 -0700

    clerk endpoint

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  // Routes that can be accessed while signed out
   publicRoutes: ["/", "/api/webhooks/clerk", "/api/webhooks/stripe"],
 });
 
 export const config = {
-  // Protects all routes, including api/trpc.
-  // https://clerk.com/docs/references/nextjs/auth-middleware
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
