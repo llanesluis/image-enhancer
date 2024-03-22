8a346ea100a7de5f999870568550f4c3052ea129
Luis Llanes <luisllaboj@gmail.com>
2024-03-21 17:22:28 -0700

    auth routes metadata

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,17 @@
 import { ReactNode } from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Autenticación",
+  description:
+    "Inicie sesión o cree una cuenta para acceder a tu cuenta de Pickuro.",
+};
 
 interface AuthLayoutProps {
   children: ReactNode;
 }
 
 export default function AuthLayout({ children }: AuthLayoutProps) {
-  //from-[#f553d766] via-[#68faff66] to-[#e5ff0066]
   return (
     <main className="min-w-screen flex min-h-screen  items-center justify-center bg-gradient-to-tr from-[#144FEB44] via-transparent to-[#5784FF44] p-8">
       {children}
