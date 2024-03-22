75c935767c84c21defbe6a5927fffb1c645db983
Luis Llanes <luisllaboj@gmail.com>
2024-03-21 17:15:45 -0700

    metadata para auth pages

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,6 +3,12 @@
 import { SignIn } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { useTheme } from "next-themes";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Iniciar sesión",
+  description: "Inicia sesión para acceder a tu cuenta de Pickuro.",
+};
 
 export default function SignInPage() {
   const { resolvedTheme } = useTheme();
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,12 @@
 import { SignUp } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { useTheme } from "next-themes";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Crear cuenta",
+  description: "Crea una cuenta para acceder a Pickuro.",
+};
 export default function SignUpPage() {
   const { resolvedTheme } = useTheme();
   return (
